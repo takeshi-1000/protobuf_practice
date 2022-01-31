df5e6d3873f3862ca385b67a31e6740494295d75
komoritakeshi <komoritakeshi@team-lab.com>
2022-02-01 00:24:11 +0900

    xcodeプロジェクト新規作成

---
+++ protobuf_practice/protobuf_practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  protobuf_practice
+//
+//  Created by 小森　武史 on 2022/02/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
